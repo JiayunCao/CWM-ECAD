3140e84941bd18fd6ed07206a354c7d237a6bed0
JiayunCao <57303199+JiayunCao@users.noreply.github.com>
2020-06-09 21:53:11 +0800

    Update top.v

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,75 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+module fsm (
+            input  clk,
+				input  rst,
+				input  button,
+				
+				output  reg [2:0]  throw 
+
+            ); 
+
+
+
+
+
+
+always @ (posedge clk or negedge rst )
+begin
+   if(!rst)
+	throw<=3'b001;
+	else 
+ begin	
+	   case(throw)
+		
+	 3'b001:
+	         begin
+		       if(button) 
+				 throw <= 3'b010;
+				 else
+				 throw <= throw;
+				end
+	 3'b010:
+	        begin
+		       if(button) 
+				 throw <= 3'b011;
+				 else
+				 throw <= throw;
+			  end
+	 3'b011:
+	       begin
+		       if(button) 
+				 throw <= 3'b100;
+				 else
+				 throw <= throw;
+			 end
+	 3'b100:
+	       begin
+		       if(button) 
+				 throw <= 3'b101;
+				 else
+				 throw <= throw;
+			end
+	 3'b101:
+	      begin
+		       if(button) 
+				 throw <= 3'b110;
+				 else
+				 throw <= throw;
+		   end
+	 3'b110:	
+         begin 
+            if(button) 
+				 throw <= 3'b001;
+				 else
+				 throw <= throw; 
+			end
+	 
+     default : throw <=3'b001;
+	endcase
+  end
+end
+	
+
+endmodule
