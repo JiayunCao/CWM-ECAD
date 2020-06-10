7bace50acfcf5f72361f5cc1dbf2bc36e43bb6e7
JiayunCao <57303199+JiayunCao@users.noreply.github.com>
2020-06-10 23:25:17 +0800

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,48 @@
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns/100ps
+`define clk_t 20
+module red_tb;
 
+
+reg    clk;
+wire   red;
+wire   amber;
+wire   green;
+
+
+
+
+
+red test(
+
+
+    .clk(clk),
+    .red(red),
+    .amber(amber),
+    .green(green)
+	 
+	 
+    );
+
+
+
+
+
+				  				 
+initial
+begin
+
+					clk=1;
+
+					#20000;
+					$stop;
+
+ 
+
+end
+ 
+always #(`clk_t/2) clk=~clk;
+
+endmodule
