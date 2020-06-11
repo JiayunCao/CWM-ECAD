c621e8c0ad0113b33d3164407f21cba59e795a5e
JiayunCao <57303199+JiayunCao@users.noreply.github.com>
2020-06-11 20:43:18 +0800

    Update top.v

--- Ex7/top.v
+++ Ex7/top.v
@@ -14,3 +14,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+module mult  (  
+					input        clk,
+					input  [2:0] a,
+					inout  [2:0] b, 
+					input        enable,
+					
+					output reg [5:0] result=6'b0
+				);
+				
+
+	integer i;        
+	
+		always @(posedge clk)               
+		begin
+			if(enable)
+			begin
+				for(i=0;i<=2;i=i+1)
+				if(b[i])
+				result<=result+(a<<(i-1)) ;   
+		   end
+		end
+		
+		
+
+  
+endmodule
